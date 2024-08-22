c51e8fcc473baece2cf3ae2aa444d7cd800fdb8c
Wilhelm Widergold <widergold.wilhelm@outlook.de>
2024-08-22 14:05:13 +0200

    add persistence plugin to neovim, add just treesitter support and add neovide settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+if vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+  vim.g.neovide_scale_factor = 1.1
+  vim.g.gui_font_default_size = 16
+  vim.g.gui_font_size = vim.g.gui_font_default_size
+  vim.g.gui_font_face = "FiraCode Nerd Font Mono"
+  vim.o.guifont = string.format("%s:h%s", vim.g.gui_font_face, vim.g.gui_font_size)
+end
