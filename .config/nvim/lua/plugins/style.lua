476345d4d303683922f00f866342d6285d6d0583
Wilhelm Widergold <widergold.wilhelm@outlook.de>
2024-08-22 14:03:57 +0200

    change neovim theme to flow

--- .config/nvim/lua/plugins/style.lua
+++ .config/nvim/lua/plugins/style.lua
@@ -1,15 +1,16 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = true,
+    "0xstepit/flow.nvim",
+    lazy = false,
     priority = 1000,
-    opts = { style = "mocha" },
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
+    opts = {},
+    config = function()
+      require("flow").setup({
+        fluo_color = "pink",
+        mode = "normal",
+        aggressive_spell = false,
+      })
+      vim.cmd("colorscheme flow")
+    end,
   },
 }
