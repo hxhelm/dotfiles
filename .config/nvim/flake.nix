aa83abc7269c9058efe10174f697ac2347c94918
Wilhelm Widergold <widergold.wilhelm@outlook.de>
2025-06-03 20:49:55 +0200

    add nix development shell for lua and lua language server

--- .config/nvim/flake.nix
+++ .config/nvim/flake.nix
@@ -3,20 +3,26 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: {
-    devShells.default = let
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-      };
-    in pkgs.mkShell {
-      packages = with pkgs; [
-        lua
-        lua-language-server
-      ];
+  outputs = { self, nixpkgs, ... }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in f pkgs
+      );
+    in {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            lua
+            lua-language-server
+          ];
 
-      shellHook = ''
-        echo "Lua dev shell ready 🐉"
-      '';
+          shellHook = ''
+            echo "🐉 Lua dev shell loaded"
+            '';
+        };
+      });
     };
-  };
 }
