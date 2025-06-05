327a553f80180979ef940552d62f6788e3b22e4e
Wilhelm Widergold <widergold.wilhelm@outlook.de>
2025-06-05 15:01:34 +0200

    lint: formatting

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,4 @@
 return {
-  { "mason-org/mason.nvim", enabled = false },
+  { "mason-org/mason.nvim",           enabled = false },
   { "mason-org/mason-lspconfig.nvim", enabled = false },
 }
--- .config/nvim/lua/plugins/projects.lua
+++ .config/nvim/lua/plugins/projects.lua
@@ -11,7 +11,7 @@ return {
   init = function()
     -- enable saving the state of plugins in the session
     vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
-    vim.opt.sessionoptions:remove("folds") -- fix for https://github.com/Shatur/neovim-session-manager/issues/133
+    vim.opt.sessionoptions:remove("folds")   -- fix for https://github.com/Shatur/neovim-session-manager/issues/133
   end,
   dependencies = {
     { "nvim-lua/plenary.nvim" },
@@ -22,6 +22,6 @@ return {
   priority = 100,
   keys = {
     { "<leader>pe", "<cmd>Telescope neovim-project discover<cr>", desc = "Explore your projects" },
-    { "<leader>ph", "<cmd>Telescope neovim-project history<cr>", desc = "Browse your project history" },
+    { "<leader>ph", "<cmd>Telescope neovim-project history<cr>",  desc = "Browse your project history" },
   },
 }
