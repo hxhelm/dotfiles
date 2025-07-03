46e2e78ec912e1394a30ab5bec048fb78d9322a8
Wilhelm Widergold <widergold.wilhelm@outlook.de>
2025-07-03 15:05:43 +0200

    remove neovide config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,25 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- neovide setup
-if vim.g.neovide then
-  vim.g.neovide_scale_factor = 1.1
-  vim.g.gui_font_default_size = 16
-  vim.g.gui_font_size = vim.g.gui_font_default_size
-  vim.g.gui_font_face = "FiraCode Nerd Font Mono"
-  vim.o.guifont = string.format("%s:h%s:#e-subpixelantialias", vim.g.gui_font_face, vim.g.gui_font_size)
-
-  vim.g.neovide_cursor_animation_length = 0
-  vim.g.neovide_scroll_animation_length = 0
-
-  vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
-  vim.keymap.set("v", "<D-c>", '"+y')    -- Copy
-
-  vim.keymap.set({ "n", "v", "s", "x", "o", "i", "l", "c", "t" }, "<D-v>", function()
-    vim.api.nvim_paste(vim.fn.getreg("+"), true, -1)
-  end, { noremap = true, silent = true })
-end
-
 -- misc settings
 if vim.fn.has('macunix') == 1 then
   vim.g.clipboard = "pbcopy"
